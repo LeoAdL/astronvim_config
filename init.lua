78f408e9a0e6e47cf0c9e889423e5f0a689b2663
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2022-12-16 22:57:59 -0600

    Integration Notify

--- init.lua
+++ init.lua
@@ -262,7 +262,102 @@ local config = {
                         --     require("lsp_signature").setup()
                         --   end,
                         -- },
-                        ["catppuccin/nvim"] = { as = catppuccin },
+                        ["catppuccin/nvim"] = {
+                                as = catppuccin,
+                                config = function()
+                                        require("catppuccin").setup {
+                                                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                                                background = { -- :h background
+                                                        light = "latte",
+                                                        dark = "mocha",
+                                                },
+                                                transparent_background = true,
+                                                term_colors = false,
+                                                dim_inactive = {
+                                                        enabled = false,
+                                                        shade = "dark",
+                                                        percentage = 0.15,
+                                                },
+                                                no_italic = false, -- Force no italic
+                                                no_bold = false, -- Force no bold
+                                                styles = {
+                                                        comments = { "italic" },
+                                                        conditionals = { "italic" },
+                                                        loops = {},
+                                                        functions = {},
+                                                        keywords = {},
+                                                        strings = {},
+                                                        variables = {},
+                                                        numbers = {},
+                                                        booleans = {},
+                                                        properties = {},
+                                                        types = {},
+                                                        operators = {},
+                                                },
+                                                color_overrides = {},
+                                                custom_highlights = {},
+                                                integrations = {
+                                                        cmp = true,
+                                                        gitsigns = true,
+                                                        nvimtree = true,
+                                                        telescope = true,
+                                                        notify = true,
+                                                        mini = false,
+                                                        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                                                },
+                                        }
+
+                                        -- setup must be called before loading
+                                        vim.cmd.colorscheme "catppuccin"
+                                end,
+                        },
+                        ["rebelot/kanagawa.nvim"] = {},
+                        ["rose-pine/neovim"] = {
+                                as = "rose-pine",
+                                config = function()
+                                        require("rose-pine").setup {
+                                                --- @usage 'main' | 'moon'
+                                                dark_variant = "moon",
+                                                bold_vert_split = false,
+                                                dim_nc_background = false,
+                                                disable_background = false,
+                                                disable_float_background = false,
+                                                disable_italics = false,
+
+                                                --- @usage string hex value or named color from rosepinetheme.com/palette
+                                                groups = {
+                                                        background = "base",
+                                                        panel = "surface",
+                                                        border = "highlight_med",
+                                                        comment = "muted",
+                                                        link = "iris",
+                                                        punctuation = "subtle",
+
+                                                        error = "love",
+                                                        hint = "iris",
+                                                        info = "foam",
+                                                        warn = "gold",
+
+                                                        headings = {
+                                                                h1 = "iris",
+                                                                h2 = "foam",
+                                                                h3 = "rose",
+                                                                h4 = "gold",
+                                                                h5 = "pine",
+                                                                h6 = "foam",
+                                                        },
+                                                        -- or set all headings at once
+                                                        -- headings = 'subtle'
+                                                },
+
+                                                -- Change specific vim highlight groups
+                                                highlight_groups = {
+                                                        ColorColumn = { bg = "rose" },
+                                                },
+                                        }
+                                        vim.cmd "colorscheme rose-pine"
+                                end,
+                        },
                         ["simrat39/symbols-outline.nvim"] = {
                                 config = function() require("symbols-outline").setup() end,
                         },
